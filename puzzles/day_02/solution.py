1fda37c4039d04886ef65e043a1c469c3a97417b
dylan-ansel <d.ansel.richardson@gmail.com>
2022-12-02 19:35:38 -0500

    p2 solution (needs cleanup)

--- puzzles/day_02/solution.py
+++ puzzles/day_02/solution.py
@@ -5,22 +5,44 @@ WINPOINTS = 6
 DRAWPOINTS = 3
 LOSSPOINTS = 0
 
-#Rock       (A, X)      1 point
-#Paper      (B, Y)      2 points
-#Scissors   (C, Z)      3 points
+# Rock       (A, X)      1 point
+# Paper      (B, Y)      2 points
+# Scissors   (C, Z)      3 points
+
+# Expected outcomes (p2)
+# Loss   X   1
+# Draw   Y   2
+# Win    Z   3
+
+wldtable = [
+[1,1,2],        # [them, us, outcome]
+[1,2,3],
+[1,3,1],
+[2,1,1],
+[2,2,2],
+[2,3,3],
+[3,1,3],
+[3,2,1],
+[3,3,2]
+]
 
 def p1():
     score = 0
     for round in INPUT.split('\n'):
         them = handvalue(round.split(' ')[0])
         us = handvalue(round.split(' ')[1])
-        score += us                                 # add the flat value of your played hand
-        if them == us:                              # draw condition
-            score += DRAWPOINTS
-        elif them - us == -1 or them - us == 2:     # win condition
-            score += WINPOINTS
-        else:                                       # loss condition
-            score += LOSSPOINTS
+        score += playhand(them, us)
+    return score
+
+def p2():
+    score = 0
+    for round in INPUT.split('\n'):
+        them = handvalue(round.split(' ')[0])
+        outcome = handvalue(round.split(' ')[1])    # expected outcome
+        for k in wldtable:
+            if k[0] == them and k[2] == outcome:
+                us = k[1]
+        score += playhand(them, us)
     return score
 
 def handvalue(alphain):
@@ -31,4 +53,16 @@ def handvalue(alphain):
     else:
         return 3
 
+def playhand(them, us):                         # TODO actually use wldtable for this
+    score = 0
+    score += us                                 # add the flat value of your played hand
+    if them == us:                              # draw condition
+        score += DRAWPOINTS
+    elif them - us == -1 or them - us == 2:     # win condition
+        score += WINPOINTS
+    else:                                       # loss condition
+        score += LOSSPOINTS
+    return score                                
+
 print(f"Your total score is {p1()}.")
+print(f"Upon reflection, your score is actually {p2()}.")
