19386f731bcc31100793a6c83a96d16256482aa5
dylan-ansel <d.ansel.richardson@gmail.com>
2022-12-04 00:38:09 -0500

    p1 solution (slightly less shitty)

--- puzzles/day_04/solution.py
+++ puzzles/day_04/solution.py
@@ -6,19 +6,12 @@ def p1():
     count = 0
     pairs = []
     for line in INPUT.split("\n"):
-        firststr = line.split(",")[0].split("-")
-        secondstr = line.split(",")[1].split("-")
-
-        first = [int(item) for item in firststr]
-        second = [int(item) for item in secondstr]
-
+        first = [int(item) for item in line.split(",")[0].split("-")]
+        second = [int(item) for item in line.split(",")[1].split("-")]
         if first[0] <= second[0] and first[1] >= second[1]:
-            #print(f"{first}, {second}")
             count += 1
         elif second[0] <= first[0] and second[1] >= first[1]:
-            #print(f"{first}, {second}")
             count += 1
-    #print(pairs)
     return count
 
 
