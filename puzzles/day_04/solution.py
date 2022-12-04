d634bedeeecf11c1f7a0f59a930f2f2c5b3b9a36
dylan-ansel <d.ansel.richardson@gmail.com>
2022-12-04 01:24:44 -0500

    p2 solution

--- puzzles/day_04/solution.py
+++ puzzles/day_04/solution.py
@@ -4,20 +4,38 @@ with open('input.txt', 'r') as f:
 
 def p1():
     count = 0
-    pairs = []
-    for line in INPUT.split("\n"):
-        first = [int(item) for item in line.split(",")[0].split("-")]
-        second = [int(item) for item in line.split(",")[1].split("-")]
-        if first[0] <= second[0] and first[1] >= second[1]:
+    assignments = parse(INPUT)
+
+    for pair in assignments:
+        if pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1]:
             count += 1
-        elif second[0] <= first[0] and second[1] >= first[1]:
+        elif pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]:
             count += 1
     return count
 
 
 def p2():
-    return ""
+    count = 0
+    assignments = parse(INPUT)
+
+    for pair in assignments:
+        first = range(pair[0][0], pair[0][1] + 1)
+        second = range(pair[1][0], pair[1][1] + 1)
+        firstset = set(first)
+
+        if len(firstset.intersection(second)) > 0: count += 1
+
+    return count
+
+
+def parse(raw):
+    assignments = []
+    for line in raw.split("\n"):
+        first = [int(item) for item in line.split(",")[0].split("-")]
+        second = [int(item) for item in line.split(",")[1].split("-")]
+        assignments.append([first, second])
+    return assignments
 
 
 if __name__ == '__main__':
-    print(p1(), p2())
+    print(f"p1: {p1()}, p2: {p2()}")
