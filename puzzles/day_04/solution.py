24c782e4e101dcd8bfc491b890b0ac47169344ba
dylan-ansel <d.ansel.richardson@gmail.com>
2022-12-04 00:34:00 -0500

    p1 solution (shitty)

--- puzzles/day_04/solution.py
+++ puzzles/day_04/solution.py
@@ -6,18 +6,20 @@ def p1():
     count = 0
     pairs = []
     for line in INPUT.split("\n"):
-        first = line.split(",")[0].split("-")
-        second = line.split(",")[1].split("-")
-        if int(first[0]) <= int(second[0]) and int(first[1]) >= int(second[1]):
-            print(f"{first}, {second}")
+        firststr = line.split(",")[0].split("-")
+        secondstr = line.split(",")[1].split("-")
+
+        first = [int(item) for item in firststr]
+        second = [int(item) for item in secondstr]
+
+        if first[0] <= second[0] and first[1] >= second[1]:
+            #print(f"{first}, {second}")
             count += 1
-        elif int(second[0]) <= int(first[0]) and int(second[1]) >= int(first[1]):
-            print(f"{first}, {second}")
+        elif second[0] <= first[0] and second[1] >= first[1]:
+            #print(f"{first}, {second}")
             count += 1
     #print(pairs)
     return count
-
-    # TODO figure out why i had to cast all this shit
 
 
 def p2():
